b7d30828e4096d022e2547013234fced6555e970
L.Lin <brooklyntechlin@gmail.com>
2016-01-25 14:39:38 -0500

    updated classes

--- Calculator/scripts/app.js
+++ Calculator/scripts/app.js
@@ -1,6 +1,39 @@
 $(document).ready(function() {
-  $("button").on("click", function() {
+  var values = [], str = "";
+  var disp = $("#disp");
+
+  $(".num").on("click", function() {
+    //alert($(this).text());
+    if($("#disp").val().length <= 15) {
+      $("#disp").val($("#disp").val() + $(this).text());
+    }
     $(".highlight").removeClass("highlight");
     $(this).addClass("highlight");
   });
+  $("#add").on("click", function() {
+    values.push(disp.val());
+    clearDisplay();
+  });
+  $("#minus").on("click", function() {
+    values.push(disp.val());
+    clearDisplay();
+  });
+
+  $("#clear").on("click", function() {
+    clearDisplay();
+  })
+
+  $("#eval").on("click", function() {
+    alert("Equals!");
+  });
+
+
+  function clearDisplay() {
+    disp.val("");
+  }
+
+  function calculate(val) {
+    return eval(val);
+  }
+
 });
