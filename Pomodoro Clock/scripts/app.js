aedbbf1dd10fb8c66e3d3ec903069c9b6e6a2973
L.Lin <brooklyntechlin@gmail.com>
2016-02-04 14:30:08 -0500

    testing buttons

--- Pomodoro Clock/scripts/app.js
+++ Pomodoro Clock/scripts/app.js
@@ -1,3 +1,18 @@
-$(document).ready(function() {
+'use strict';
 
-})
+$(document).ready(function() {
+  var time = 12;
+  const maxTime = 60;
+  const maxHour = 12;
+
+  $(".count").on("click", function() {
+    alert($(this).val() + ".count is working.");
+  });
+  $(".reset").on("click", function() {
+    alert($(this).val() + ".reset is working.");
+  });
+
+  $(".add").on("click", function() {
+    alert($(this).val() + ".add is working.");
+  })
+});
