c82977ccc2a2013e54a68b374d01a45227830cdd
L.Lin <brooklyntechlin@gmail.com>
2016-02-06 18:43:01 -0500

    updated app js

--- Pomodoro Clock/scripts/app.js
+++ Pomodoro Clock/scripts/app.js
@@ -1,18 +1,33 @@
 'use strict';
 
 $(document).ready(function() {
-  var time = 12;
-  const maxTime = 60;
-  const maxHour = 12;
+  function startTimer(duration, display) {
+    var start = Date.now(),
+        diff,
+        minutes,
+        seconds;
+    function timer() {
+      diff = duration - (((Date.now() - start) / 1000) | 0);
 
-  $(".count").on("click", function() {
-    alert($(this).val() + ".count is working.");
-  });
-  $(".reset").on("click", function() {
-    alert($(this).val() + ".reset is working.");
-  });
+      minutes = (diff / 60 ) | 0;
+      seconds = (diff % 60 ) | 0;
 
-  $(".add").on("click", function() {
-    alert($(this).val() + ".add is working.");
-  })
+      minutes = (minutes < 10 ) ? "0" + minutes : minutes;
+      seconds = (seconds < 10 ) ? "0" + seconds : seconds;
+
+      display.text(minutes + ":" + seconds);
+
+      if(diff <= 0) {
+        start = Date.now() + 1000;
+      }
+    };
+    timer();
+    setInterval(timer, 1000);
+  };
+
+  $(".start").on("click", function() {
+    var fiveMinutes = 60 * 5,
+        display = $("#time");
+    startTimer(fiveMinutes, display);
+  });
 });
