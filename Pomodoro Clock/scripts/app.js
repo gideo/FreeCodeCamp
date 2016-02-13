0fcfc1b7d8979ff969baf4b1ed0777a5c4356c6b
Tong Lin <brooklyntechlin@gmail.com>
2016-02-12 21:53:11 -0500

    added plus and minus buttons

--- Pomodoro Clock/scripts/app.js
+++ Pomodoro Clock/scripts/app.js
@@ -1,7 +1,7 @@
-$(document).ready(function() {
+$(document).ready(function() { 
   var intervalTimer;
   var setTime = [...$("#time").text().split(":")].reduce( (a,b) => parseInt(a * 60) + parseInt(b));
-  function alterTime() {
+  function alterTime() { 
     var getMinutes = (parseInt(setTime/60).toString().length < 2) ? "0" + parseInt(setTime/60): parseInt(setTime/60);
     var getSeconds = ((setTime % 60).toString().length < 2) ? "0" + (setTime % 60): (setTime % 60);
     $("#time").text(getMinutes + ":" + getSeconds);
@@ -13,15 +13,15 @@ $(document).ready(function() {
         seconds;
     function timer() {
       diff = duration - (((Date.now() - start) / 1000) | 0);
-
+      
       minutes = (diff / 60 ) | 0;
       seconds = (diff % 60 ) | 0;
-
+      
       minutes = (minutes < 10 ) ? "0" + minutes : minutes;
       seconds = (seconds < 10 ) ? "0" + seconds : seconds;
-
+        
       display.text(minutes + ":" + seconds);
-
+      
       if(diff <= 0) {
         start = Date.now() + 1000;
       }
@@ -29,18 +29,24 @@ $(document).ready(function() {
     timer();
     intervalTimer = setInterval(timer, 1000);
   };
-
+  
   $(".start").on("click", function() {
     var display = $("#time");
     startTimer(setTime, display);
   });
-
+  
   $(".add").on("click", function() {
     clearInterval(intervalTimer);
     setTime += 30;
     alterTime();
   });
-
+  $(".sub").on("click", function() {
+    clearInterval(intervalTimer);
+    if(setTime >= 60){
+      setTime -= 30;  
+    }
+    alterTime();
+  });
   $(".reset").on("click", function() {
     clearInterval(intervalTimer);
     setTime = 300;
