da2a26da52e43a87e1f585f071e79e45d3de307d
L.Lin <brooklyntechlin@gmail.com>
2016-02-22 15:36:55 -0500

    updated getPosition()

--- LocalWeather/scripts/app.js
+++ LocalWeather/scripts/app.js
@@ -1,32 +1,33 @@
 $(document).ready(function() {
   var x = $("#writeMe");
-  var longitude, latitude;
+  var appid = "3bbd55527ee1ad0e5f326312904163d3";
+  var longitude, latitude, weatherApi;
   (function(){
     if(navigator.geolocation){
       x.text("Geolocation success.");
-      navigator.geolocation.getCurrentPosition(showPosition);
+      navigator.geolocation.getCurrentPosition(getPosition);
     } else {
       x.text("Geolocation is not supported by this browser.");
     }
 
   })();
 
-  function showPosition(position) {
+  function getPosition(position) {
     longitude = position.coords.longitude;
     latitude = position.coords.latitude
-    $("#long").text("Latitude: " + longitude)
-    $("#lati").text("Longitude: " + latitude);
-  }
+    $("#long").text("Latitude: " + latitude)
+    $("#lati").text("Longitude: " + longitude);
+    weatherApi = "http://api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longitude+"&appid="+appid;
 
-//   if(navigator.geolocation) {
-//     //alert("SUCCESS");
-//     navigator.geolocation.getCurrentPosition(locationSuccess, locationError);
-//   } else {
-//     locationError("Your browser doesn't support Geolocation!");
-//   }
+    x.text(weatherApi);
+    $.getJSON(weatherApi, function (data) {
+      $.each(result, function(i, field) {
+        $("#weatherApi").text(field + " ");
+      });
+    });
+  }
 
   function requestApi() {
     var weatherApi = "http://http://api.openweathermap.org/data/2.5/weather?lat="
   }
-
 });
