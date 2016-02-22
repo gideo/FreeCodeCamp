6c1b2ef4895b6222ca2c2efaade0dff0808f0a9e
L.Lin <brooklyntechlin@gmail.com>
2016-02-22 15:04:57 -0500

    get long/lati

--- LocalWeather/scripts/app.js
+++ LocalWeather/scripts/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   var x = $("#writeMe");
-
+  var longitude, latitude;
   (function(){
     if(navigator.geolocation){
       x.text("Geolocation success.");
@@ -8,7 +8,22 @@ $(document).ready(function() {
     } else {
       x.text("Geolocation is not supported by this browser.");
     }
+
   })();
+
+  function showPosition(position) {
+    longitude = position.coords.longitude;
+    latitude = position.coords.latitude
+    $("#long").text("Latitude: " + longitude)
+    $("#lati").text("Longitude: " + latitude);
+  }
+
+//   if(navigator.geolocation) {
+//     //alert("SUCCESS");
+//     navigator.geolocation.getCurrentPosition(locationSuccess, locationError);
+//   } else {
+//     locationError("Your browser doesn't support Geolocation!");
+//   }
 
   function requestApi() {
     var weatherApi = "http://http://api.openweathermap.org/data/2.5/weather?lat="
