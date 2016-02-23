50c6138cbeaeb19b9e0925b0032e766a462a8594
L.Lin <brooklyntechlin@gmail.com>
2016-02-23 13:44:35 -0500

    got json to work

--- LocalWeather/scripts/app.js
+++ LocalWeather/scripts/app.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   var x = $("#writeMe");
   var appid = "3bbd55527ee1ad0e5f326312904163d3";
   var longitude, latitude, weatherApi;
+  var json;
   (function(){
     if(navigator.geolocation){
       x.text("Geolocation success.");
@@ -20,11 +21,22 @@ $(document).ready(function() {
     weatherApi = "http://api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longitude+"&appid="+appid;
 
     x.text(weatherApi);
-    $.getJSON(weatherApi, function (data) {
-      $.each(result, function(i, field) {
-        $("#weatherApi").text(field + " ");
+    // $.getJSON(weatherApi, function (data) {
+    //   console.log(JSON.stringify(data))
+    // });
+    json = (function() {
+      var json = null;
+      $.ajax({
+        'async': false;
+        'global': false;
+        'url': weatherApi;
+        'success': function(data) {
+          json = data;
+        }
       });
-    });
+      return json;
+    })();
+    $("#weatherApi").text(JSON.stringify(json));
   }
 
   function requestApi() {
