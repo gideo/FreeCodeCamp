63c622fd03e240d910782aadaf97bf59f414caf6
L.Lin <brooklyntechlin@gmail.com>
2016-02-25 14:05:13 -0500

    update app.js

--- LocalWeather/scripts/app.js
+++ LocalWeather/scripts/app.js
@@ -2,24 +2,24 @@ $(document).ready(function() {
   var x = $("#writeMe");
   var appid = "3bbd55527ee1ad0e5f326312904163d3";
   var longitude, latitude, weatherApi;
-  var json;
+  var json, temp, location, weather;
   (function(){
     if(navigator.geolocation){
-      x.text("Geolocation success.");
+      x.text("Geolocation pending!");
       navigator.geolocation.getCurrentPosition(getPosition);
     } else {
       x.text("Geolocation is not supported by this browser.");
     }
-    
+
   })();
-  
+
   function getPosition(position) {
     longitude = position.coords.longitude;
     latitude = position.coords.latitude
-    $("#long").text("Latitude: " + latitude) 
+    $("#long").text("Latitude: " + latitude)
     $("#lati").text("Longitude: " + longitude);
     weatherApi = "http://api.openweathermap.org/data/2.5/weather?lat="+latitude+"&lon="+longitude+"&appid="+appid;
-    
+
     x.text(weatherApi);
     json = (function() {
       var json = null;
@@ -33,22 +33,26 @@ $(document).ready(function() {
       });
       return json;
     })();
-    //$("#weatherApi").text(JSON.stringify(json));
-    $("#location").text(json["name"]);
-    $("#temperature").text(kelvinToF(json["main"]["temp"]))
+
+    temp = json["main"]["temp"];
+    location = json["name"];
+    weather = json["weather"][0]["main"];
+
+    $("#location").text(location);
+    $("#temperature").text(kelvinToF(temp));
   }
-  
+
   function kelvinToF(num){
     return (((num-273.15)*9/5)+32).toFixed(1) + "°F";
   }
   function kelvinToC(num){
     return ((num-273.15).toFixed(1)) + "°C";
   }
-  
+
   $("#btnLoc").on("click", function() {
     $("#location").text(json["name"]);
   });
-  
+
   $("#switchDeg").on("click", function() {
     var t = $("#temperature");
     if(t.text()[t.text().length-1] === "F"){
