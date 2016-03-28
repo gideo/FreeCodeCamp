2e960a4cb4722842b2a7140ce104b1d624c1f775
L.Lin <brooklyntechlin@gmail.com>
2016-03-28 15:15:11 -0400

    removed cb

--- WikipediaQuery/scripts/app.js
+++ WikipediaQuery/scripts/app.js
@@ -3,31 +3,29 @@ $(document).ready(function() {
   var search = $("#searchQuery");
   var result = $("#result");
   var api = "http://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=";
-  var cb = "&callback=JSON_CALLBACK";
   var json;
-  
+
   query.on("submit", function() {
     if(!search.val()) {
       alert("Empty!");
       return;
     }
     result.empty();
-    //alert(search.val());
     result.append("<div class='result-box result-font'>"+ search.val() + "</div>")
-    
+
     json = (function() {
       json = null;
       $.ajax({
-        'async': true,
+        'async': false,
         'global': false,
-        'url': api+search.val()+cb,
+        'url': api+search.val(),
         'success': function(data) {
           alert("sucess");
           json = data;
         }
       });
       return json;
-    })(); 
+    })();
   })
-  
+
 })
