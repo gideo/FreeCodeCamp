f899cc136ba1bb00eba6c616f2f93bd4725b327d
Tong Lin <brooklyntechlin@gmail.com>
2016-03-27 21:49:33 -0400

    added ajax call

--- WikipediaQuery/scripts/app.js
+++ WikipediaQuery/scripts/app.js
@@ -2,6 +2,9 @@ $(document).ready(function() {
   var query = $("#query");
   var search = $("#searchQuery");
   var result = $("#result");
+  var api = "http://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=";
+  var cb = "&callback=JSON_CALLBACK";
+  var json;
   
   query.on("submit", function() {
     if(!search.val()) {
@@ -9,7 +12,22 @@ $(document).ready(function() {
       return;
     }
     result.empty();
-    alert(search.val());
+    //alert(search.val());
     result.append("<div class='result-box result-font'>"+ search.val() + "</div>")
+    
+    json = (function() {
+      json = null;
+      $.ajax({
+        'async': true,
+        'global': false,
+        'url': api+search.val()+cb,
+        'success': function(data) {
+          alert("sucess");
+          json = data;
+        }
+      });
+      return json;
+    })(); 
   })
+  
 })
