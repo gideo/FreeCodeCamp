8752490e8036806ca191ebdf15079d14efdee672
Tong Lin <brooklyntechlin@gmail.com>
2016-03-29 22:43:41 -0400

    finally got json

--- WikipediaQuery/scripts/app.js
+++ WikipediaQuery/scripts/app.js
@@ -2,9 +2,11 @@ $(document).ready(function() {
   var query = $("#query");
   var search = $("#searchQuery");
   var result = $("#result");
-  var api = "http://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=";
+  var apix = "http://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=";
+  var api = "http://en.wikipedia.org/w/api.php?format=json&action=query&titles=";
+  var cb = "&prop=revisions&rvprop=content&callback=?";
   var json;
-
+  
   query.on("submit", function() {
     if(!search.val()) {
       alert("Empty!");
@@ -12,20 +14,25 @@ $(document).ready(function() {
     }
     result.empty();
     result.append("<div class='result-box result-font'>"+ search.val() + "</div>")
-
-    json = (function() {
+    $.getJSON(api+search.val()+cb ,function(data) {
+      json = data.query.pages;
+      alert(JSON.stringify(json, null, 4));
+    });
+  })
+  
+  /*
+  json = (function() {
       json = null;
       $.ajax({
-        'async': false,
+        'async': true,
         'global': false,
         'url': api+search.val(),
         'success': function(data) {
           alert("sucess");
-          json = data;
+          json = data.query.pages;
         }
       });
       return json;
-    })();
-  })
-
+    })(); 
+  */
 })
