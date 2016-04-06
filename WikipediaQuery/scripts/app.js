b65b830b831228cbef80160e96ba28f792faf54f
Tong Lin <brooklyntechlin@gmail.com>
2016-04-05 21:20:53 -0400

    angularized

--- WikipediaQuery/scripts/app.js
+++ WikipediaQuery/scripts/app.js
@@ -1,5 +1,33 @@
 var app = angular.module('MainApp', ['ngAnimate']);
-
+app.controller('Controller', function($scope, $http, $timeout) {
+  var query = $("#query");
+  var search = $("#searchQuery");
+  var result = $("#result");
+  var api = "http://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=";
+  var cb = "&prop=revisions&rvprop=content&callback=?";
+  var json;
+  
+  function emptyResult() {
+    result.empty();
+  }
+  
+  query.on("submit", function() {
+    emptyResult();
+    let query = search.val();
+    if(!query) {
+      alert("Empty!");
+      return;
+    }
+    result.append("<div class='result-box result-font'>"+ query + "</div>")
+    $.getJSON(api+query+cb ,function(data) {
+    json = data.query.pages;
+    //$(".result-box").append("<div class='result-font'>"+ JSON.stringify(json,null, 4) + "</div>")
+    alert(JSON.stringify(json, null, 4));
+    });
+  })
+ 
+});
+/*
 $(document).ready(function() {
   var query = $("#query");
   var search = $("#searchQuery");
@@ -26,4 +54,4 @@ $(document).ready(function() {
     alert(JSON.stringify(json, null, 4));
     });
   })
-})
+})*/
