96edc4debdf6e67d8ea4d6f42bb65fb6b1a1069e
Tong Lin <brooklyntechlin@gmail.com>
2016-04-09 22:12:47 -0400

    added angular file

--- WikipediaQuery/scripts/app.js
+++ WikipediaQuery/scripts/app.js
@@ -1,5 +1,34 @@
+// var app = angular.module('MainApp', ['ngAnimate']);
+// app.controller('Controller', function($scope, $http, $timeout) {
+//   var query = $("#query");
+//   var search = $("#searchQuery");
+//   var result = $("#result");
+//   var api = "http://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=";
+//   var cb = "&prop=revisions&rvprop=content&callback=?";
+//   var json;
+  
+//   function emptyResult() {
+//     result.empty();
+//   }
+  
+//   query.on("submit", function() {
+//     emptyResult();
+//     let query = search.val();
+//     if(!query) {
+//       alert("Empty!");
+//       return;
+//     }
+//     result.append("<div class='result-box result-font'>"+ query + "</div>")
+//     $.getJSON(api+query+cb ,function(data) {
+//     json = data.query.pages;
+//     alert(JSON.stringify(json, null, 4));
+//     });
+//   })
+// });
+
 var app = angular.module('MainApp', ['ngAnimate']);
-app.controller('Controller', function($scope, $http, $timeout) {
+
+$(document).ready(function() {
   var query = $("#query");
   var search = $("#searchQuery");
   var result = $("#result");
@@ -21,8 +50,8 @@ app.controller('Controller', function($scope, $http, $timeout) {
     result.append("<div class='result-box result-font'>"+ query + "</div>")
     $.getJSON(api+query+cb ,function(data) {
     json = data.query.pages;
+    //$(".result-box").append("<div class='result-font'>"+ JSON.stringify(json,null, 4) + "</div>")
     alert(JSON.stringify(json, null, 4));
     });
   })
- 
-});
+})
