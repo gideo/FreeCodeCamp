e3a2dd202feb8d0a0d327dd46f819d3c2f83ad47
Tong Lin <brooklyntechlin@gmail.com>
2016-04-14 23:23:37 -0400

    removed comments

--- WikipediaQuery/scripts/app.js
+++ WikipediaQuery/scripts/app.js
@@ -1,31 +1,3 @@
-// var app = angular.module('MainApp', ['ngAnimate']);
-// app.controller('Controller', function($scope, $http, $timeout) {
-//   var query = $("#query");
-//   var search = $("#searchQuery");
-//   var result = $("#result");
-//   var api = "http://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=";
-//   var cb = "&prop=revisions&rvprop=content&callback=?";
-//   var json;
-  
-//   function emptyResult() {
-//     result.empty();
-//   }
-  
-//   query.on("submit", function() {
-//     emptyResult();
-//     let query = search.val();
-//     if(!query) {
-//       alert("Empty!");
-//       return;
-//     }
-//     result.append("<div class='result-box result-font'>"+ query + "</div>")
-//     $.getJSON(api+query+cb ,function(data) {
-//     json = data.query.pages;
-//     alert(JSON.stringify(json, null, 4));
-//     });
-//   })
-// });
-
 var app = angular.module('MainApp', ['ngAnimate']);
 
 $(document).ready(function() {
@@ -50,7 +22,6 @@ $(document).ready(function() {
     result.append("<div class='result-box result-font'>"+ query + "</div>")
     $.getJSON(api+query+cb ,function(data) {
     json = data.query.pages;
-    //$(".result-box").append("<div class='result-font'>"+ JSON.stringify(json,null, 4) + "</div>")
     alert(JSON.stringify(json, null, 4));
     });
   })
